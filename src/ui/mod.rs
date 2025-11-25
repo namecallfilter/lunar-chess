f33dd8c369a93b6978949f9b09b11b138e89f258
kaoru <namecallfilter@users.noreply.github.com>
2025-11-25 08:21:23 -0500

    fix: parking lot

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,9 +1,10 @@
 pub mod draw_board;
 pub mod overlay;
 
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 
 pub use overlay::{BestMove, BoardBounds, UserEvent, start_overlay};
+use parking_lot::Mutex;
 
 use crate::model::detected::{DetectedBoard, DetectedPiece};
 
